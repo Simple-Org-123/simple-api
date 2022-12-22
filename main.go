254168d54ae7c1af722c789fdd1c67df4164b97f
Brian Matheson <82474958+mathesonbr@users.noreply.github.com>
2022-12-22 15:16:38 -0500

    Change id to int, add getline

--- main.go
+++ main.go
@@ -2,24 +2,31 @@ package main
 
 import (
 	"net/http"
+	"math/rand"
 	"github.com/gin-gonic/gin"
 )
 
 // the yow struct is a collection of one-line phrases
 type yow struct {
-	ID     string `json:"id"`
+	ID     int `json:"id"`
 	Line   string `json:"line"`
 }
 
 // let's populate some yow lines
 var yowLines = []yow {
-	{ID: "1", Line: "One of the crossbeams is out of skew on the treddle"},
-	{ID: "2", Line: "Nobody expects the Spanish Inquisition"},
-	{ID: "3", Line: "Our chief weapon is surprise"},
+	{ID: 1, Line: "One of the crossbeams is out of skew on the treddle"},
+	{ID: 2, Line: "Nobody expects the Spanish Inquisition"},
+	{ID: 3, Line: "Our chief weapon is surprise"},
 }
 
 func getYowLines(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, yowLines)
+}
+
+func getYowLine(c *gin.Context) {
+	var rand = rand.Intn(len(yowLines));
+
+	c.IndentedJSON(http.StatusOK, yowLines[rand];
 }
 
 func main() {
