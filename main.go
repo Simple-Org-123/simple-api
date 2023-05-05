326a6aae891c639b83df1090a12daa902ee74152
Brian Matheson <mathesonbr@vmware.com>
2023-05-05 09:51:44 -0400

    hacks

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"net/http"
-	"math/rand"
 	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
 )
 
 // the yow struct is a collection of one-line phrases
 type yow struct {
-	ID     int `json:"id"`
-	Line   string `json:"line"`
+	ID   int    `json:"id"`
+	Line string `json:"line"`
 }
 
 // let's populate some yow lines
-var yowLines = []yow {
+var yowLines = []yow{
 	{ID: 1, Line: "One of the crossbeams is out of skew on the treddle"},
 	{ID: 2, Line: "Nobody expects the Spanish Inquisition"},
 	{ID: 3, Line: "Our chief weapon is surprise"},
@@ -34,6 +34,8 @@ func getYowLine(c *gin.Context) {
 }
 
 func main() {
+	dbcfg := mysql.Configt
+
 	router := gin.Default()
 	router.GET("/all", getYowLines)
 	router.GET("/", getYowLine)
